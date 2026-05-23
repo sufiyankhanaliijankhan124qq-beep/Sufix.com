94d6db12510f0f72736d01a63422710a3256b91e
sufiyankhanaliijankhan124qq-beep <sufiyankhanaliijankhan124qq@gmail.com>
2026-05-23 15:44:06 +0500

    Create page.jsx

---
+++ frontend/frontend/app/frontend/app/page.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import WalletPreview from "../components/WalletPreview";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+
+      <Navbar />
+
+      <Hero />
+
+      <WalletPreview />
+
+      <Footer />
+
+    </main>
+  );
+}
