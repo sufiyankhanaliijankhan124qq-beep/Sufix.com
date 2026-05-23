7f7cc2ee79fd5dc17543fb675332cb81351cabfe
sufiyankhanaliijankhan124qq-beep <sufiyankhanaliijankhan124qq@gmail.com>
2026-05-23 15:45:36 +0500

    Create Navbar.jsx

---
+++ frontend/frontend/app/frontend/components/Navbar.jsx
@@ -0,0 +1,25 @@
+export default function Navbar() {
+  return (
+
+<nav className="nav">
+
+<div className="logo">
+Sufix
+</div>
+
+<div>
+
+<button>
+Login
+</button>
+
+<button>
+Start
+</button>
+
+</div>
+
+</nav>
+
+);
+}
